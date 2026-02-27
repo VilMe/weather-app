0935761a461c9dbb1a5a96393d36b616e6baa24f
VilMe <vilma.medrano00@gmail.com>
2026-02-26 22:35:42 -0600

    complete class Weather and def string method for Weather class

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ import os
 
 
 API_KEY: Final[str] = os.environ['OPEN_WEATHER_API_KEY']
-BASE_URL: Final[str] = 'api.openweathermap.org/data/2.5/forecast'
+BASE_URL: Final[str] = 'https://api.openweathermap.org/data/2.5/forecast'
+
