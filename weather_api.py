b3ae4f5a95ba7b1f723cd8abbeae6061134b6fb9
VilMe <vilma.medrano00@gmail.com>
2026-02-28 23:37:41 -0600

    define get weather funnction for weather api class

---
+++ weather_api.py
@@ -0,0 +1,14 @@
+from typing import Final
+import os
+import requests
+
+
+
+API_KEY: Final[str] = os.environ['OPEN_WEATHER_API_KEY']
+BASE_URL: Final[str] = 'https://api.openweathermap.org/data/2.5/forecast'
+
+
+def get_weather(city_name: str, mock: bool = True) -> dict:
+    if mock: 
+        with opent('dummy_data.json') as file:
+            return json.load(file)
