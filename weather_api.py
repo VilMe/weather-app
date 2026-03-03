0b6a6be9cb5c695d88ff772ca5efd53bcad857ee
VilMe <vilma.medrano00@gmail.com>
2026-03-02 21:38:32 -0600

    define how to get live data for caching

--- weather_api.py
+++ weather_api.py
@@ -10,5 +10,9 @@ BASE_URL: Final[str] = 'https://api.openweathermap.org/data/2.5/forecast'
 
 def get_weather(city_name: str, mock: bool = True) -> dict:
     if mock: 
-        with opent('dummy_data.json') as file:
+        with open('dummy_data.json') as file:
             return json.load(file)
+        
+
+        # request live data
+        payload: dict = {'q': city_name, 'appid': API_key, 'units': 'metric'}
