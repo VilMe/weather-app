1d6f3ad30059f83746e35e10ba298f3502accd11
VilMe <vilma.medrano00@gmail.com>
2026-03-19 22:47:39 -0500

    add list of weather forecast for the days returned

--- weather_api.py
+++ weather_api.py
@@ -1,4 +1,5 @@
 from typing import Final
+from model import Weather, dt
 import os
 import requests
 
@@ -30,6 +31,15 @@ def get_weather_details(weather: dict) -> list[Weather]:
 
     if not days:
         raise Exception(f'Problem with json: {weather}')
+    
+
+    list_of_weather: list[Weather] = []
+    for day in days:
+        w: Weather = Weather(date=dt.fromtimestamp(day.get('dt')),
+                             details=(details := day.get('main')),
+                             temp=details.get('temp'),
+                             weather=(weather := day.get('weather')),
+                             description=weather[0].get('descripton'))
 
 
 if __name__ == '__main__':
